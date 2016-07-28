960bbe64ee7d3a5b36919fbf739b23ecedb56275
Manoj Chalode <chalodem@gmail.com>
2016-07-28 23:26:06 +0530

    added a to do app for loki js demo

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var loki = require('lokijs');
+
+var db = new loki('loki.json');
+var todo = db.addCollection('todo');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.json(todo.data.map(function(item) {return {
+		title: item.title,
+		description: item.description
+	}}));
+});
+
+router.post('/', function (req, res, next) {
+	var data = req.body;
+	todo.insert(data);
+	res.json(data);
 });
 
 module.exports = router;
